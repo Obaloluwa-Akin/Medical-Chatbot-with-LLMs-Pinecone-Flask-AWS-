d589e328a613ea45fcc8dffa0d264728a8def5be
Obaloluwa <obaloluwa89@gmail.com>
2026-02-10 02:34:47 +0100

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Obaloluwa Akinyemi Precious",
+    author_email="obaloluwa89@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
